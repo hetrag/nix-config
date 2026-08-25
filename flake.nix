03f1129bd95c3a1b4a8e3810fc06abc15859d702
Your Name <you@example.com>
2026-08-25 20:56:45 +0200

    a

--- flake.nix
+++ flake.nix
@@ -1,12 +1,20 @@
-outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
-  nixosConfigurations = {
+{
+  description = "NixOS Configuration Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:nixos/nixos-hardware/master";
+};
+
+  outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
+    nixosConfigurations = {
     
-    # Laptop
-    laptop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
+      # Laptop
+      laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
         nixos-hardware.nixosModules.lenovo-thinkpad-p14s-amd-gen2
-        ./modules/core
+        ./modules
         ./modules/desktop-env   # <--- Shared GUI
         ./hosts/laptop
       ];
