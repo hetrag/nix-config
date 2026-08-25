3754787a5533f182f0e898144a17e6ee6648239b
Your Name <you@example.com>
2026-08-25 20:57:20 +0200

    s

---
+++ modules/desktop-env/default.nix
@@ -0,0 +1,34 @@
+{ pkgs, ... }:
+
+{
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "dk";
+    variant = "";
+  };
+
+  # Audio (PipeWire)
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Common GUI Packages for both machines
+  environment.systemPackages = with pkgs; [
+    vlc
+    kdePackages.kate
+    google-chrome
+  ];
+}
+
+ 
