14b2f88f9d3fed34c25c9f80266a949969bc8f96
Your Name <you@example.com>
2026-08-31 15:52:39 +0200

    fix: restructure qdrant configuration to encapsulate settings under services.qdrant

--- modules/services/qdrant.nix
+++ modules/services/qdrant.nix
@@ -1,17 +1,22 @@
 { ... }:
 
 {
-  hnsw_index = {
-    on_disk = true;
+  services.qdrant = {
+    enable = true;
+    settings = {
+      hnsw_index = {
+        on_disk = true;
+      };
+      service = {
+        grpc_port = 6334;
+        host = "0.0.0.0";
+        http_port = 6333;
+      };
+      storage = {
+        snapshots_path = "/data/databases/qdrant/snapshots";
+        storage_path = "/data/databases/qdrant/storage";
+      };
+      telemetry_disabled = true;
+    };
   };
-  service = {
-    grpc_port = 6334;
-    host = "0.0.0.0";
-    http_port = 6333;
-  };
-  storage = {
-    snapshots_path = "/data/databases/qdrant/snapshots";
-    storage_path = "/data/databases/qdrant/storage";
-  };
-  telemetry_disabled = true;
 }
